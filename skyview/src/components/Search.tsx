09118d9968b99e668d0c60bbe85d2665a7b333f1
Emma Jensen <emmasarahlinnea@gmail.com>
2024-07-16 12:41:05 +0200

    suggestionlist absolute to not move other content

--- skyview/src/components/Search.tsx
+++ skyview/src/components/Search.tsx
@@ -111,20 +111,21 @@ const Search: React.FC<SearchProps> = ({
             >
                Search
             </button>
+            {/*             {loadingSuggestions && <p>Loading suggestions...</p>}
+ */}            {suggestedCities.length > 0 && (
+               <div className="absolute left-0 mt-1 w-full max-w-md bg-white shadow-lg rounded-lg overflow-hidden z-10">
+                  <ul className="divide-y divide-gray-200">
+                     {suggestedCities.map((city, index) => (
+                        <li key={index} className="px-4 py-2 hover:bg-gray-100 cursor-pointer" onClick={() => handleSuggestionClick(city)}>
+                           {city}
+                        </li>
+                     ))}
+                  </ul>
+               </div>
+            )}
          </div>
-         {loadingSuggestions && <p>Loading suggestions...</p>}
-         {suggestedCities.length > 0 && (
-            <div className="mt-1 w-full max-w-md bg-white shadow-lg rounded-lg overflow-hidden">
-               <ul className="divide-y divide-gray-200">
-                  {suggestedCities.map((city, index) => (
-                     <li key={index} className="px-4 py-2 hover:bg-gray-100 cursor-pointer" onClick={() => handleSuggestionClick(city)}>
-                        {city}
-                     </li>
-                  ))}
-               </ul>
-            </div>
-         )}
       </form>
+
    );
 };
 
